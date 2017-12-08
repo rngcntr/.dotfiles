b9c27b6489681c15f4259dbd3f72b652a8f45b95
Florian Grieskamp <f.grieskamp@gmail.com>
2017-12-08 15:59:49 +0100

    Removec conky files

--- .config/conky/conky-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-conky -q -c /home/rngcntr/.config/conky/system.conf &
-conky -q -c /home/rngcntr/.config/conky/cpu.conf &
-conky -q -c /home/rngcntr/.config/conky/space.conf &
-conky -q -c /home/rngcntr/.config/conky/net.conf &
-conky -q -c /home/rngcntr/.config/conky/time.conf & exit
